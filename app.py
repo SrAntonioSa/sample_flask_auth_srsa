d128aa5430b9125d6b22554beecd80796e650c17
SrAntonioSa <antonio.m.sa@hotmail.com>
2025-06-18 16:22:58 +0200

    june 18 25 update

--- app.py
+++ app.py
@@ -118,14 +118,8 @@ def delete_user(id_user):
         
     return jsonify({"message": " Usuario nao encontrado"}),404
  
-         
-     
-     
 
 
-@app.route("/hello-world", methods=["GET"])
-def hello_world():
-    return " Hello World"
 
 if __name__== '__main__':
     app.run(debug=True)
