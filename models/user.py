85648460f980b55419d4225c7b49d3893f28fcec
SrAntonioSa <antonio.m.sa@hotmail.com>
2025-06-20 18:05:53 +0200

    implementing role for diferent users permissions

--- models/user.py
+++ models/user.py
@@ -8,4 +8,6 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable = False, default="user")
+
