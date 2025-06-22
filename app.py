89a603cba193ebf526a9fb62d8668fa4d04f342e
SrAntonioSa <antonio.m.sa@hotmail.com>
2025-06-22 14:10:53 +0200

    implementing password encryption with bcrypt

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request,jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
+
 
 
 app = Flask(__name__)
@@ -31,8 +33,11 @@ def create_user():
      username = data.get("username")
      password = data.get("password")
 
+
      if username and password:
-          user = User(username = username, password = password, role='user' )
+          
+          hashed_password= bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+          user = User(username = username, password = hashed_password  , role='user' )
           db.session.add(user)
           db.session.commit()
 
@@ -55,7 +60,7 @@ def login():
             
             user = User.query.filter_by(username=username).first()
 
-            if user and user.password== password:
+            if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
                     login_user(user)
                     print(current_user.is_authenticated)
                     return jsonify({"message": "Autenticaçao realizada com sucesso "})
